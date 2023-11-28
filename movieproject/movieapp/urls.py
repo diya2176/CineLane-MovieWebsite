11271fe7190fe2ec3b12ea3f87ba6d5eb5dbcbe7
Diya Mereen Thomas <56103312+diya2176@users.noreply.github.com>
2023-11-28 19:14:47 +0400

    Updated urls.py

--- movieproject/movieapp/urls.py
+++ movieproject/movieapp/urls.py
@@ -6,10 +6,10 @@ from . import views
 app_name='movieapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('movie/<int:movie_id>/',views.detail, name='detail'),
-    path('add/',views.add_movie, name='add_movie'),
-    path('update/<int:id>/',views.update, name='update'),
-    path('delete/<int:id>/',views.delete, name='delete')
+    path('movie/<int:movie_id>/', views.detail, name='detail'),
+    path('add/', views.add_movie, name='add_movie'),  # Added 'name' parameter
+    path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
 
 if settings.DEBUG:
